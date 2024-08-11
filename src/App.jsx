7bb8cea0eb1c03ec87a1c54ea02c8297a1af60b8
jenvuong <jennifervuong4@gmail.com>
2024-08-12 10:57:51 +1200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className="text-center text-3xl font-bold">Vite, React and TailwindCSS template :)</h1>
+    </>
+  )
+}
+
+export default App
