f0ec8bf361837123685e1b874f14ead63a657f4b
jenvuong <jennifervuong4@gmail.com>
2024-08-15 22:51:15 +1200

    cleaned up template

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
-
-
 function App() {
-
-
   return (
     <>
-    <h1 className="text-center text-3xl font-bold">Vite, React and TailwindCSS template :)</h1>
+      <div className="h-screen flex items-center justify-center">
+        <h1 className="text-center text-3xl font-bold">
+          Vite, React and TailwindCSS template! <br></br> :)
+        </h1>
+      </div>
     </>
   )
 }
