261b3444eb164543afd395b3185ce334ff82032e
jenvuong <jennifervuong4@gmail.com>
2024-08-15 22:33:00 +1200

    Revert "updates"
    This reverts commit 2e9c5a126657d22e087dd0a2102f93a9a08e16bd.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
+import { useState } from 'react'
+
+
 function App() {
+
+
   return (
     <>
-      <section className="hero p-12">
-        <div className="container mx-auto flex h-full items-center justify-center p-6 px-6 text-white">
-          <div className="mx-auto rounded bg-white/20 p-6 backdrop-blur-lg">
-            <h2 className="text-center text-2xl font-semibold text-white">
-              Current Timezone
-            </h2>
-          </div>
-        </div>
-      </section>
+    <h1 className="text-center text-3xl font-bold">Vite, React and TailwindCSS template :)</h1>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
